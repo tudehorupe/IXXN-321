4ef87dfff3292d506d5eb20ff74d7cc550ecb324
Rupert <rupert4t@gmail.com>
2020-03-28 15:12:24 +1300

    week 3 updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import Header from './components/Header';
+import ContactCard from './components/ContactCard';
+import data from './data/employee.json';
+
+
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <header className="App-header">
+
+
+      <div className="cardBox">
+      {data.Employees.map(i => <ContactCard userImg={i.userImg} firstName={i.firstName} lastName={i.lastName} jobTitle={i.jobTitle} region={i.region} phoneNumber={i.phoneNumber} />)}
+      </div>
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload a good time.
         </p>
         <a
           className="App-link"
